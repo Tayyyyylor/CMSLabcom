04f1de12df5e620709de0f9c785e8420e1ab4774
Tayyyyylor <bryan.h.pro@gmail.com>
2024-11-23 17:18:11 +0100

    config for ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 // import type { Core } from '@strapi/strapi';
 
+import { Core } from "@strapi/strapi";
+
 export default {
   /**
    * An asynchronous register function that runs before
@@ -7,8 +9,10 @@ export default {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/* { strapi }: { strapi: Core.Strapi } */) {},
-
+ 
+  register( { strapi }: { strapi: Core.Strapi } ) {
+  },
+  
   /**
    * An asynchronous bootstrap function that runs before
    * your application gets started.

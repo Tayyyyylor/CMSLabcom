b3812b5048ab6c6bb9e9cd6fe9fcfe172d156299
Tayyyyylor <bryan.h.pro@gmail.com>
2024-11-23 18:10:43 +0100

    for the front

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,6 +2,19 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'https://labcom-optimaa.com', 
+        'https://www.labcom-optimaa.com',       
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+      allowedHeaders: '*',                     
+      exposedHeaders: '*',                   
+      credentials: false,                        
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
